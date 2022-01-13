a15b03634d2c4bc7876ca462aa0a97809fa52181
WGB5445 <919603023@qq.com>
2022-01-14 01:14:41 +0800

    创建测试项

--- stcns-web/src/views/Home.vue
+++ stcns-web/src/views/Home.vue
@@ -4,7 +4,12 @@
     <button type="button" @click="test">链接</button>
     <button type="button" @click="init">init</button>
     <button type="button" @click="register">register</button>
-    <button type="button" @click="call">call</button>
+    <button type="button" @click="call">解析</button>
+    <button type="button" @click="call1">解析全部</button>
+    <button type="button" @click="change">修改解析地址</button>
+    <button type="button" @click="send">发送</button>
+    <button type="button" @click="renewal">续费</button>
+    
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
@@ -27,7 +32,9 @@ export default {
   },
   data(){
     return {
-        starcoinProvider:null
+        starcoinProvider:null,
+        admin:"0xc17e245c8ce8dcfe56661fa2796c98cf",
+        name: "Tim12345678910"
       }
   }
   ,
@@ -52,7 +59,7 @@ export default {
     },
     async init(){
       try {
-        const functionId = '0xb87b4d228705e6e481dd21538a819371::STCNS_script::init'
+        const functionId = this.admin+'::STCNS_script::init'
         const strTypeArgs = []
          const tyArgs = utils.tx.encodeStructTypeTags(strTypeArgs)
 
@@ -122,7 +129,7 @@ export default {
     },
     async register(){
       try {
-        const functionId = '0xb87b4d228705e6e481dd21538a819371::STCNS_script::register'
+        const functionId = this.admin+'::STCNS_script::register'
         const strTypeArgs = []
          const tyArgs = utils.tx.encodeStructTypeTags(strTypeArgs)
 
@@ -133,7 +140,10 @@ export default {
           return false
         }
         console.log({ toAccount })
-        
+             // const args = [
+        //   arrayify(toAccount),
+        //   arrayify(amountSCSHex),
+        // ]
         // let n = 0.01 
         // const sendAmount = parseFloat(n, 10)
         // if (!(sendAmount > 0)) {
@@ -157,9 +167,9 @@ export default {
         })()
         
         // console.log({ sendAmountHex, sendAmountNanoSTC: sendAmountNanoSTC.toString(10), amountSCSHex })
-        let domain = "Tim"
+
         const args = [
-          "Tim12345678911",
+          this.name,
           "1"
         ]
         console.log("stes")
@@ -189,23 +199,184 @@ export default {
       }
     },
     async call(){
+           const args = [
+            "b\""+this.name+"\"",
+         
+        ]
        this.starcoinProvider.send(
               'contract.call_v2',
               [
                 {
-                  function_id: "0xb87b4d228705e6e481dd21538a819371::STCNS_script::init",
+                  function_id: this.admin+'::STCNS_script::resolution_stcaddress',
                   type_args: [],
-                  args:[],
+                  args:["x\"54696d3132333435363738393130\""],
                 },
               ],
             ).then((result) => {
               
-                console.log(result[0])
+                console.log(result)
 
 
             })
           
-    }
+    },
+    async call1(){
+           const args = [
+            "b\""+this.name+"\"",
+         
+        ]
+       this.starcoinProvider.send(
+              'contract.call_v2',
+              [
+                {
+                  function_id: this.admin+'::STCNS_script::get_domain_info',
+                  type_args: [],
+                  args:["x\"54696d3132333435363738393130\""],
+                },
+              ],
+            ).then((result) => {
+              
+                console.log(result)
+
+
+            })
+          
+    },
+    async send(){
+      try {
+        const functionId = this.admin+'::STCNS_script::send'
+        const strTypeArgs = []
+         const tyArgs = utils.tx.encodeStructTypeTags(strTypeArgs)
+
+        const amountSCSHex = (function () {
+          const se = new bcs.BcsSerializer()
+          // eslint-disable-next-line no-undef
+          se.serializeU64(BigInt("1"))
+          return hexlify(se.getBytes())
+        })()
+        
+        // console.log({ sendAmountHex, sendAmountNanoSTC: sendAmountNanoSTC.toString(10), amountSCSHex })
+
+        const args = [
+          this.name,
+          "0x57b052EA2d156B01A2596969E7aa4044"
+        ]
+        console.log("stes")
+        const scriptFunction =await utils.tx.encodeScriptFunctionByResolve(functionId, tyArgs, args,'https://barnard-seed.starcoin.org')
+        console.log(scriptFunction)
+
+        // Multiple BcsSerializers should be used in different closures, otherwise, the latter will be contaminated by the former.
+        const payloadInHex = (function () {
+          const se = new bcs.BcsSerializer()
+          scriptFunction.serialize(se)
+          return hexlify(se.getBytes())
+        })()
+        console.log({ payloadInHex })
+
+        const txParams = {
+          data: payloadInHex,
+        }
+
+
+
+        console.log({ txParams })
+        const transactionHash = await this.starcoinProvider.getSigner().sendUncheckedTransaction(txParams)
+        console.log({ transactionHash })
+      } catch (error) {
+
+        throw error
+      }
+    },
+    async change(){
+      try {
+        const functionId = this.admin+'::STCNS_script::change_Resolver_stcaddress'
+        const strTypeArgs = []
+         const tyArgs = utils.tx.encodeStructTypeTags(strTypeArgs)
+
+        const amountSCSHex = (function () {
+          const se = new bcs.BcsSerializer()
+          // eslint-disable-next-line no-undef
+          se.serializeU64(BigInt("1"))
+          return hexlify(se.getBytes())
+        })()
+        
+        // console.log({ sendAmountHex, sendAmountNanoSTC: sendAmountNanoSTC.toString(10), amountSCSHex })
+
+        const args = [
+          this.name,
+          "0x57b052EA2d156B01A2596969E7aa4044"
+        ]
+        console.log("stes")
+        const scriptFunction =await utils.tx.encodeScriptFunctionByResolve(functionId, tyArgs, args,'https://barnard-seed.starcoin.org')
+        console.log(scriptFunction)
+
+        // Multiple BcsSerializers should be used in different closures, otherwise, the latter will be contaminated by the former.
+        const payloadInHex = (function () {
+          const se = new bcs.BcsSerializer()
+          scriptFunction.serialize(se)
+          return hexlify(se.getBytes())
+        })()
+        console.log({ payloadInHex })
+
+        const txParams = {
+          data: payloadInHex,
+        }
+
+
+
+        console.log({ txParams })
+        const transactionHash = await this.starcoinProvider.getSigner().sendUncheckedTransaction(txParams)
+        console.log({ transactionHash })
+      } catch (error) {
+
+        throw error
+      }
+    },
+    async renewal(){
+      try {
+        const functionId = this.admin+'::STCNS_script::renewal_domain'
+        const strTypeArgs = []
+         const tyArgs = utils.tx.encodeStructTypeTags(strTypeArgs)
+
+        const amountSCSHex = (function () {
+          const se = new bcs.BcsSerializer()
+          // eslint-disable-next-line no-undef
+          se.serializeU64(BigInt("1"))
+          return hexlify(se.getBytes())
+        })()
+        
+        // console.log({ sendAmountHex, sendAmountNanoSTC: sendAmountNanoSTC.toString(10), amountSCSHex })
+
+        const args = [
+          this.name,
+          "1"
+        ]
+        console.log("stes")
+        const scriptFunction =await utils.tx.encodeScriptFunctionByResolve(functionId, tyArgs, args,'https://barnard-seed.starcoin.org')
+        console.log(scriptFunction)
+
+        // Multiple BcsSerializers should be used in different closures, otherwise, the latter will be contaminated by the former.
+        const payloadInHex = (function () {
+          const se = new bcs.BcsSerializer()
+          scriptFunction.serialize(se)
+          return hexlify(se.getBytes())
+        })()
+        console.log({ payloadInHex })
+
+        const txParams = {
+          data: payloadInHex,
+        }
+
+
+
+        console.log({ txParams })
+        const transactionHash = await this.starcoinProvider.getSigner().sendUncheckedTransaction(txParams)
+        console.log({ transactionHash })
+      } catch (error) {
+
+        throw error
+      }
+    },
   }
 }
 </script>
